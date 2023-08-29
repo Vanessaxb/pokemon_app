4dca4e051bd6afa816e6011f7748a19cfd65c28b
Vanessa Beattie <vanessabeattie@Vanessas-MacBook-Pro.local>
2023-08-28 22:56:19 -0500

    Created show views of each pokemon. HM Complete

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,41 +1,44 @@
 //shows full list of POkemon from Data array
 
-const React = require('react')
+const React = require("react");
 
 const myStyle = {
-    color: '#ffffff',
-    backgroundColor: '#000000',
-    };
+  color: "red",
+//   backgroundColor: "#000000",
+};
 
-function Index (props) {
-    const {pokemonData} = props;
-    console.log(pokemonData);
+function Index(props) {
+  const { pokemonData } = props;
+  console.log(pokemonData);
 
-    return (
-    <main>
-        <h1>See all the Pokemon! </h1>
-        
-        <ul>
-            {pokemonData.map((pokemon, i) => {
-                return (
-                    <li>
-                        <a href={`/pokemon/${i}`}>
-                        {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
-                        </a>
-                    </li>
-                )
-            })}
-        </ul>
-        </main>
-    )
+  return (
+    <main> 
+      <h1>See all the Pokemon! </h1>
+
+      <ul>
+        {pokemonData.map((pokemon, i) => {
+          return (
+            <li>
+              <a href={`/pokemon/${i}`} style={myStyle}>
+                {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
 
+module.exports = Index;
 
-    
-// class MyFirstComponent extends React.Component() {
-// return (
-//     <div style={myStyle}>My First React Component!</div>;
-// )
-// }
+//!Not sure where to add this
+//
 
-   module.exports = Index; 
+//   class MyFirstComponent extends React.Component {
+//     render() {
+//       return (
+//         <div style={myStyle}>My First React Component!</div>
+//       );
+//     }
+//   }
--- views/Show.jsx
+++ views/Show.jsx
@@ -2,14 +2,32 @@
 
 const React = require("react");
 
+const mainStyles = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+  position: "relative"
+};
+
+const backLinkStyles = {
+  color: "blue",
+  textDecoration: "none",
+  marginTop: "10px",
+  fontSize: "20px", 
+  position: "absolute",
+  top: "10px", 
+  left: "10px", 
+};
+
 function Show(props) {
   const { pokemon } = props;
 
   return (
-    <main>
-      <a href={`/pokemon`}>Back</a>
+    <main style={mainStyles}>
+      <a href={`/pokemon`} style={backLinkStyles}>Back</a>
       <h1>Gotta Catch 'Em All</h1>
-      <h2> {pokemon.name} </h2>
+      <h1> {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)} </h1>
       <img src={pokemon.img + ".jpg"} alt={pokemon.name}></img>
     </main>
   );
